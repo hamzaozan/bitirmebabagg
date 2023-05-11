2a4f90116ad6514eff3ffab6fb428143c1b38789
b21784807 <44296769+b21784807@users.noreply.github.com>
2023-05-11 19:32:40 +0300

    dnd updated

--- src/Components/DnD.js
+++ src/Components/DnD.js
@@ -4,44 +4,33 @@ import {Button, Checkbox, Input} from "antd";
 
 const finalSpaceCharacters = [
   {
-    id: 'gary',
+    id: 'button',
     name: 'Gary Goodspeed',
     thumb: '/images/gary.png'
   },
   {
-    id: 'cato',
+    id: 'chckbx',
     name: 'Little Cato',
     thumb: '/images/cato.png'
   },
   {
-    id: 'kvn',
+    id: 'input',
     name: 'KVN',
     thumb: '/images/kvn.png'
   },
-  {
-    id: 'mooncake',
-    name: 'Mooncake',
-    thumb: '/images/mooncake.png'
-  },
-  {
-    id: 'quinn',
-    name: 'Quinn Ergon',
-    thumb: '/images/quinn.png'
-  }
 ]
 
 function DnD() {
   const [characters, updateCharacters] = useState(finalSpaceCharacters);
-
   function handleOnDragEnd(result) {
     if (!result.destination) return;
-    console.log(finalSpaceCharacters)
-
     const items = Array.from(characters);
     const [reorderedItem] = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, reorderedItem);
 
     updateCharacters(items);
+    characters.push({id:'input',name:'okan',thumb:''})
+    console.log(characters);
   }
 
   return (
@@ -57,11 +46,9 @@ function DnD() {
                     <Draggable key={id} draggableId={id} index={index}>
                       {(provided) => (
                         <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                          {id==='gary' && <Button/>}
-                          {id==='cato' && <Checkbox/>}
-                          {id==='quinn' && <Input/>}
-                          {id==='kvn' && <Button/>}
-                          {id==='mooncake' && <Button/>}
+                          {id==='button' && <Button/>}
+                          {id==='chckbx' && <Checkbox/>}
+                          {id==='input' && <Input/>}
                         </li>
                       )}
                     </Draggable>
